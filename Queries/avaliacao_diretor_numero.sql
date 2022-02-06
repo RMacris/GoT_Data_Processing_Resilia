f49c4b6eaef92e0c1b8c2be17933299898c11e18
Rafael Soares <rafa.macris@gmail.com>
2022-02-06 14:43:28 -0300

    typo corretion; new queries

--- Queries/avaliacao_diretor_numero.sql
+++ Queries/avaliacao_diretor_numero.sql
@@ -1,3 +1,3 @@
--- Avaliação media dos diretores com relação ao numero de episodios Dirigidos
+-- 3 - Avaliação media dos diretores com relação ao numero de episodios Dirigidos
 Select distinct Director, avg(Rating) , count(Director) From episodes 
 Group by Director order by Rating desc;
--- Queries/menores_notas_temporada.sql
+++
@@ -1,3 +0,0 @@
--- Quais temporadas tiveram as maiores notas?
-Select `Season`, avg(`Rating`) as `medias_notas` from `episodes`
-group by `Season` order by `medias_notas` asc;
