62bbb7dc1738705f80a69d27673af7c2e6681f4a
fabianobarroli <fabianobarroli@gmail.com>
2022-02-08 22:54:55 -0300

    corrigindo os graficos

--- queries/quantos personagens por casa.sql
+++ queries/quantos personagens por casa.sql
@@ -1,4 +1,4 @@
 use got;
 
 select house, count(perso) as Casa from characters
-where house like '%' group by house;
+where house in ('Baratheon','Tully', 'Arryn', 'Martell', 'Greyjoy', 'Tyrell', 'Targaryen','Lannister', 'Stark', 'Bolton', 'Morahe') group by house order by Casa desc;
