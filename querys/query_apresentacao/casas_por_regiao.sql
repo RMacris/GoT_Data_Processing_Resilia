effc01de781a0adc902f68dcf737872e7f35a641
fabianobarroli <fabianobarroli@gmail.com>
2022-02-11 16:20:00 -0300

    correcao dos nomes dos arquivos

--- querys/query_apresentacao/casas_por_regiao.sql
+++ querys/query_apresentacao/casas_por_regiao.sql
@@ -1,7 +1,5 @@
 use got;
 
--- 1 - Quantas casas tem por região
-
 select distinct House_name, id, Region from houses order by Region, House_name;
 
 select Region, count(Region) from houses group by Region;
